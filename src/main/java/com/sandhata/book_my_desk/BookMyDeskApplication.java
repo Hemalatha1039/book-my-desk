92d083a425f8d927e08559b302716da75efa63a6
hemalapa <hemalatha1039@gmail.com>
2024-09-09 22:40:00 +0530

    Initial Commit

---
+++ src/main/java/com/sandhata/book_my_desk/BookMyDeskApplication.java
@@ -0,0 +1,13 @@
+package com.sandhata.book_my_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandhata/book_my_desk/BookMyDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandhata.book_my_desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
